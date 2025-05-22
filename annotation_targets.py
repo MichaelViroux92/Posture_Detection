7ed294dc4d8f9fb38b3932aa2359efbdaa910da4
MichaelViroux92 <michael.viroux@gmail.com>
2025-05-22 23:02:31 +0200

    Initial mediapipe file

--- annotation_targets.py
+++ annotation_targets.py
@@ -27,6 +27,30 @@ class Annotater:
                 continue
         self.image_filenames = sorted(os.listdir(self.raw_images_folder_path))
 
+    
+    def validate_input(self):
+        input_values_head = [-1, 'Forward head', 0, 'Neutral head']
+        input_values_neck = [-1, 'Flexion', 0, 'Neutral', 1, 'Extension']
+        input_values_shoulder = [-1, 'Protracted', 0, 'Neutral']
+        input_values_thoraric_spine = [0, 'Neutral', 1, 'Hyperkyfosis']
+        input_values_lumbar_spine = [0, 'Neutral', 1, 'Hyperlordosis']
+        annotation_values = {
+                    'head':input_values_head,
+                    'neck': input_values_neck,
+                    'shoulder': input_values_shoulder,
+                    'thoraric_spine': input_values_thoraric_spine,
+                    'lumbar_spine': input_values_lumbar_spine,
+        }
+        
+
+        for body_part, annotation_value in annotation_values.items():
+            while True:
+                user_input = int(input('Annotate '))
+                if int(input()) != parameters[i]:
+                    print("Wrong input, enter again")
+                else:
+                    continue
+    
     def annotate_images(self):
         self.annotated_data = {}
         previous_status = None
@@ -58,20 +82,20 @@ class Annotater:
                     os.remove(raw_image_path)
                     previous_status = f"Image deleted"
                 else:
-                    annotate_shoulder = int(input('Annotate shoulder position (-1, 0, 1): '))
-                    annotate_neck = int(input('Annotate neck position (-1, 0, 1): '))
-                    annotate_thoraric_spine = int(input('Annotate thoraric spine (0, 1): '))
-                    annotate_lumbar_spine = int(input('Annotate lumar spine (0, 1): '))
-                    annotate_pelvis = int(input('Annotate pelvis position (0, 1): '))
+                    annotate_head = int(input('Annotate head position (-1: Forward head, 0: Neutral head): '))
+                    annotate_neck = int(input('Annotate neck position (-1: Flexion, 0: Neutral, 1: Extension): '))
+                    annotate_shoulder = int(input('Annotate shoulder position (-1: Protracted, 0: Neutral): '))
+                    annotate_thoraric_spine = int(input('Annotate thoraric spine (0: Neutral, 1: Hyperkyfosis): '))
+                    annotate_lumbar_spine = int(input('Annotate lumar spine (0: Neutral, 1: Hyperlordosis): '))
                     
-                    self.annotated_data[image_filename] = [annotate_shoulder, annotate_neck, annotate_thoraric_spine, annotate_lumbar_spine, annotate_pelvis]
+                    self.annotated_data[image_filename] = [annotate_head, annotate_neck, annotate_shoulder, annotate_thoraric_spine, annotate_lumbar_spine]
 
                     previous_status = f"Image annotated"
 
                     
 
     def save_annotated_images(self):
-        columns = ['Shoulder', 'Neck', 'Thoraric_Spine', 'Lumbar_Spine', 'Pelvis']
+        columns = ['Head', 'Neck', 'Shoulder', 'Thoraric_Spine', 'Lumbar_Spine']
         self.df_annotated_images = pd.DataFrame.from_dict(self.annotated_data, orient='index', columns=columns)
         self.df_annotated_images = self.df_annotated_images.reset_index().rename(columns={'index': 'image_filename'})
         self.df_annotated_images.to_csv('annotated_images', encoding='utf-8', index=False)
