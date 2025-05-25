6b4e716eab8d0a89769d3daf6d22ac90ad7fee98
MichaelViroux92 <michael.viroux@gmail.com>
2025-05-25 14:01:27 +0200

    Added initial feature engineering class

--- spinepose_images.py
+++ spinepose_images.py
@@ -4,28 +4,26 @@ import matplotlib.pyplot as plt
 import os
 
 def main():
-    # Initialize estimator to run on CPU
-    estimator = SpinePoseEstimator(device='cpu')
-
-    # Adjust the image path to your actual image location
+    estimator = SpinePoseEstimator(device='cpu') # change to gpu later
     image_path = os.path.join(os.getcwd(), 'data', 'raw_images', 'archive', 'images', 'train', 'Image_0.jpg')
 
-    # Read image with OpenCV
     image = cv2.imread(image_path)
     if image is None:
         raise FileNotFoundError(f"Image not found at {image_path}")
 
-    # Step 1: Detect people bounding boxes in the image
     bboxes = estimator.detect(image)
 
     if bboxes is None or bboxes.shape[0] == 0:
         print("No people detected in the image.")
         return
 
-    # Step 2: Estimate spine keypoints within detected bounding boxes
     keypoints, scores = estimator.estimate(image, bboxes)
+    print(keypoints)
+    
+    ##
+    # Get only keypoints of interest(spine keypoints)
+    ##
 
-    # Step 3: Visualize keypoints on the image
     visualized = estimator.visualize(image, keypoints, scores)
 
     # Save the visualized output
